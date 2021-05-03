5c57f369366807b60e1c3e48655ef5808272842f
Harshal Sinha <harshal.sinha@philips.com>
2021-05-03 22:41:01 +0530

    Corrected signin endpoint

--- controllers/signin.js
+++ controllers/signin.js
@@ -22,7 +22,7 @@ const handleSignin = (req, res, bcrypt, db) => {
             res.status(400).json(`Invalid login credentials`)
         }
     }).
-    catch(err => res.status(400).json(err))
+    catch(res.status(400).json(`Invalid login credentials`))
 }
 
 module.exports = {
